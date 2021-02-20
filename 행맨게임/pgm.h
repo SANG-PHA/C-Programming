98218fb13c4d7ac6d5122a1c8f81aaf5051fbf42
SANGPHA <68215551+SANG-PHA@users.noreply.github.com>
2021-02-21 07:30:40 +0900

    -

---
+++ 행맨게임/pgm.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#define TRUE 1
+#define FALSE 0
+#define NUM_OF_DICT 100
+#define DICT_SIZE 30
+char dict[NUM_OF_DICT][DICT_SIZE];
