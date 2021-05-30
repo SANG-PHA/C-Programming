4576202d5ebcded17eb938f4d17d4fc7b33263c7
SANGPHA <68215551+SANG-PHA@users.noreply.github.com>
2021-05-30 09:24:55 +0900

    Create ++연산자중복 외부함수.cpp

---
+++ ++연산자중복 외부함수.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+class Power {
+	int kick;
+	int punch;
+public:
+	Power(int kick = 0, int punch = 0) {
+		this->kick = kick;
+		this->punch = punch;
+	}
+
+	Power operator+(const Power& a);
+	friend Power& operator++(Power& a);
+	friend const Power operator++(Power& a, int x);
+	void show();
+};
+
+Power Power::operator+(const Power& a) {
+	Power tmp;
+	tmp.kick = this->kick + a.kick;
+	tmp.punch = this->punch + a.punch;
+	return tmp;
+}
+
+void Power::show() {
+	cout << "kick = " << kick << ", punch = " << punch << endl;
+}
+
+Power& operator++(Power& a)
+{
+	++(a.kick);
+	++(a.punch);
+	return a;
+}
+
+const Power operator++(Power& a, int x)
+{
+	Power tmp = a;
+	a.kick++;
+	a.punch++;
+	return tmp;
+}
+
+
+int main(void)
+{
+	Power a(1, 1), b(2, 2), c, d, e;
+
+	c = ++a;
+	d = b++;
+	e = c + d;
+
+	a.show();
+	b.show();
+	c.show();
+	d.show();
+	e.show();
+
+	return 0;
+}
